afe8bd0f1981a7ac82ffaaf3ff633eeb55de2b75
avi-kasliwal <avi.kasliwal04@gmail.com>
2022-04-15 09:30:46 +0530

    Debit/Credit & Unix to dateTime

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-
-import {useState} from 'react';
+import { useState } from "react";
 
 const App = () => {
-
-  const [address, setAddress] = useState('0x825206207a91480716a2dBE07aA0105bA9f0c975');
+  const [address, setAddress] = useState(
+    "0x825206207a91480716a2dBE07aA0105bA9f0c975"
+  );
   const [transactions, setTransactions] = useState([]);
 
   const handleInputChange = (e) => {
     setAddress(e.target.value);
-  }
+  };
 
   const getTransactions = async () => {
-    let tempUrl = `https://api-rinkeby.etherscan.io/api?module=account&action=tokentx&contractaddress=0xD92E713d051C37EbB2561803a3b5FBAbc4962431&address=${address}&page=1&offset=100&sort=desc&apikey=S6NDTUZUY36D92HS821DVEH7IHX3F8MMTD`
+    let tempUrl = `https://api-rinkeby.etherscan.io/api?module=account&action=tokentx&contractaddress=0xD92E713d051C37EbB2561803a3b5FBAbc4962431&address=${address}&page=1&offset=100&sort=desc&apikey=S6NDTUZUY36D92HS821DVEH7IHX3F8MMTD`;
 
-    const x = await fetch (tempUrl);
+    const x = await fetch(tempUrl);
     const resp = await x.json();
 
-    setTransactions (resp.result);
+    setTransactions(resp.result);
+  };
 
+  const getDate = (unix) => {
+    let t = new Date(unix * 1000);
 
-    console.log (transactions);
-  } 
+    return t.toLocaleString();
+  };
 
   return (
     <div className="App">
@@ -28,52 +31,47 @@ const App = () => {
 
       <hr />
 
-      <input onChange = {handleInputChange} value = {address}/>
-      <button onClick = {getTransactions}>Submit </button>
+      <input onChange={handleInputChange} value={address} />
+      <button onClick={getTransactions}>Submit </button>
 
-      <div> 
+      <div>
+        <h2> Debits </h2>
+        {transactions
+          .filter((txn) => txn.from.toUpperCase() === address.toUpperCase())
+          .map((txn) => {
+            return (
+              <div key={txn.hash}>
+                <h4> {txn.blockNumber} </h4>
+                <p> from : {txn.from} </p>
+                <p> to : {txn.to} </p>
+                <p>
+                  value : {txn.value} {txn.tokenSymbol}
+                </p>
 
-      <h2> Debits </h2>
-      {
-        transactions.map ((txn) => {
-         return  <div key = {txn.hash}> 
-              <h4> {txn.blockNumber} </h4>
-              <p> from : { txn.from } </p>
-              <p> to  : { txn.to } </p> 
-              <p> value : { (txn.value)} {txn.tokenSymbol}</p>
+                <p>time :{getDate(txn.timeStamp)}</p>
+              </div>
+            );
+          })}
 
-
-              {let hours = Math.floor(txn.timeStamp / 60 / 60)
-            let minutes = Math.floor((txn.timeStamp - hours * 60 * 60) / 60)
-            let seconds = Math.floor(txn.timeStamp - hours * 60 * 60 - minutes * 60 )
-            let duration = hours + ':' + minutes + ':' + seconds}
-
-<p> time : { duration } </p>
-            </div>
-        
-        })
-      }
-
-      {/* <h2> Credits </h2>
-
-      {
-        transactions.map ((txn) => {
-          return (
-
-            <div key = {txn.hash}> 
-              <h2> {txn.blockNumber} </h2>
-              <p> from : { txn.from } </p>
-              <p> to  : { txn.to } </p> 
-              <p> value : { (txn.value)} {(txn.value !== ' ') && txn.tokenSymbol}</p>
-            </div>
-          )
-        })
-      } */}
-
+        <h2> Credits </h2>
+        {transactions
+          .filter((txn) => txn.from.toUpperCase() !== address.toUpperCase())
+          .map((txn) => {
+            return (
+              <div key={txn.hash}>
+                <h4> {txn.blockNumber} </h4>
+                <p> from : {txn.from} </p>
+                <p> to : {txn.to} </p>
+                <p>
+                  value : {txn.value} {txn.tokenSymbol}
+                </p>
+                <p>time :{getDate(txn.timeStamp)}</p>
+              </div>
+            );
+          })}
       </div>
-
     </div>
   );
-}
+};
 
 export default App;

a19d6c83fbba5bdc098a2628a7934fc5549a5bb4
Yash Nahta <yashnahtanaman12345@gmail.com>
2022-04-15 13:00:20 +0530

    install web3 and npm install --save ethers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
 import { useState } from "react";
+import { Button, Card } from "react-bootstrap";
+import { ethers } from "ethers";
 
 const App = () => {
   const [address, setAddress] = useState(
     "0x825206207a91480716a2dBE07aA0105bA9f0c975"
   );
-  const [transactions, setTransactions] = useState([]);
+  const [transactions, setTransactions] = useState({
+    profit:0,
+    loss:0,
+  });
+
+  const [data, setdata] = useState({
+    address1: "",
+    Balance: null,
+  });
+
+  const btnhandler = () => {
+    // Asking if metamask is already present or not
+    if (window.ethereum) {
+      // res[0] for fetching a first wallet
+      window.ethereum
+        .request({ method: "eth_requestAccounts" })
+        .then((res) => accountChangeHandler(res[0]));
+    } else {
+      alert("install metamask extension!!");
+    }
+  };
+  const getbalance = (address) => {
+    // Requesting balance method
+    window.ethereum
+      .request({
+        method: "eth_getBalance",
+        params: [address, "latest"],
+      })
+      .then((balance) => {
+        // Setting balance
+        setdata({
+          Balance: ethers.utils.formatEther(balance),
+        });
+      });
+  };
+  const accountChangeHandler = (account) => {
+    // Setting an address data
+    setdata({
+      address1: account,
+    });
+    // Setting a balance
+    getbalance(account);
+    console.log(account);
+  };
 
   const handleInputChange = (e) => {
     setAddress(e.target.value);
@@ -30,6 +75,9 @@ const App = () => {
       <h1>Anytime Accounting</h1>
 
       <hr />
+      <Button onClick={btnhandler} variant="primary">
+        Connect to wallet
+      </Button>
 
       <input onChange={handleInputChange} value={address} />
       <button onClick={getTransactions}>Submit </button>
@@ -45,7 +93,7 @@ const App = () => {
                 <p> from : {txn.from} </p>
                 <p> to : {txn.to} </p>
                 <p>
-                  value : {txn.value} {txn.tokenSymbol}
+                  value : {txn.value / 1000000} {txn.tokenSymbol}
                 </p>
 
                 <p>time :{getDate(txn.timeStamp)}</p>
@@ -63,7 +111,7 @@ const App = () => {
                 <p> from : {txn.from} </p>
                 <p> to : {txn.to} </p>
                 <p>
-                  value : {txn.value} {txn.tokenSymbol}
+                  value : {txn.value / 1000000} {txn.tokenSymbol}
                 </p>
                 <p>time :{getDate(txn.timeStamp)}</p>
               </div>
